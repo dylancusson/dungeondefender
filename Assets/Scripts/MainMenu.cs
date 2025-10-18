59a9ed10f06736c2903fa15ad5ad922e8adccd68
Agnosthesia <157441196+Agn0sth3s1a@users.noreply.github.com>
2025-10-17 18:49:46 -0700

    Rudimentary main menu and "inventory"
    A very much ugly, and WIP main menu that more or less functions. Can close the game, start the first level, etc. Also, did some work on the in-level UI. Will do more work tonight, just wanted *something* up

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("LeveltoLoad")]
+    public string _newGameLevel;
+    private string levelToLoad;
+
+    [SerializeField] private GameObject noSavedGameDialog = null;
+
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene(_newGameLevel);
+    }
+
+    public void LoadGameDialogYes()
+    {
+        if (PlayerPrefs.HasKey("SavedLevel"))
+        {
+            levelToLoad = PlayerPrefs.GetString("SavedLevel");
+            SceneManager.LoadScene(levelToLoad);
+        }
+        else
+        {
+            noSavedGameDialog.SetActive(true);
+        }
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
